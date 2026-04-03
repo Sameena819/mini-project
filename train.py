6e0112505901d9c1ba976f48c44549706dd38ab6
Sameena <mohdsameena054@gmail.com>
2026-04-03 08:47:11 +0530

    Implement MNIST classification model training
    This script loads the MNIST dataset, preprocesses it, builds a neural network model using Keras, trains the model, and saves it to a file.

--- train.py
+++ train.py
@@ -1,1 +1,46 @@
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
 
+# Load dataset
+train = pd.read_csv("mnist_train.csv")
+test = pd.read_csv("mnist_test.csv")
+
+# Split features and labels
+X_train = train.iloc[:, 1:].values
+y_train = train.iloc[:, 0].values
+
+X_test = test.iloc[:, 1:].values
+y_test = test.iloc[:, 0].values
+
+# Normalize
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Reshape
+X_train = X_train.reshape(-1, 28, 28)
+X_test = X_test.reshape(-1, 28, 28)
+
+# One-hot encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Model
+model = Sequential([
+    Flatten(input_shape=(28,28)),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=5, batch_size=32)
+
+model.save("mnist_model.h5")
+
+print("Done!")
